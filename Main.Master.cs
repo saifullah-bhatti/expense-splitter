fd913035c4c7d0a4a0260222e935154c544c9e1a
saifullah-bhatti <144831330+saifullah-bhatti@users.noreply.github.com>
2023-12-08 20:54:35 +0500

    Add files via upload

---
+++ Main.Master.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SplitBuddy_2._0
+{
+    public partial class Main : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
+        {
+            Response.Redirect("Home.aspx");
+        }
+    }
+}
